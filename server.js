59def1191264770e08de6aead1913d31a556778c
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2019-12-15 17:05:29 -0600

    delete requests work

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "db.json"));
 });
 
-// Route that adds a new note to the db.json file
+// Takes a JSON input with key "message" and adds a new note object with that message to the db.json file
 app.post("/api/notes", function(req, res) {
     fs.readFile("./db.json", "utf8", function(error, response) {
         if (error) {
@@ -52,7 +52,34 @@ app.post("/api/notes", function(req, res) {
             if (err) throw err;
         });
     });
-})
+});
+
+// Deletes the note object with requested id from the db.json file, returns the deleted note; if no such id exists returns false
+app.delete("/api/notes/:id", function(req, res) {
+    const deleteId = req.params.id;
+    fs.readFile("db.json", "utf8", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        let notes = JSON.parse(response);
+        if (deleteId <= notes.length) {
+            res.json(notes.splice(deleteId-1,1));
+            console.log(notes);
+            // Reassign the ids of all notes
+            for (let i=0; i<notes.length; i++) {
+                notes[i].id = i+1;
+            }
+            fs.writeFile("db.json", JSON.stringify(notes, null, 2), function(err) {
+                if (err) throw err;
+            });
+        } else {
+            res.json(false);
+        }
+        
+
+    });
+    
+});
 
 // Starts the server to begin listening
 // =============================================================
