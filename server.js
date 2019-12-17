86970345cc28a41d81456cfe87d2b781dca84ccf
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2019-12-16 21:22:44 -0600

    moved front-end files to public folder, web app functionality now works

--- server.js
+++ server.js
@@ -12,18 +12,19 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user to the index page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // Basic route that sends the user to the notes page
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // Route that sends the user the db.json file
@@ -33,22 +34,21 @@ app.get("/api/notes", function(req, res) {
 
 // Takes a JSON input with key "message" and adds a new note object with that message to the db.json file
 app.post("/api/notes", function(req, res) {
-    fs.readFile("./db.json", "utf8", function(error, response) {
+    fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
         if (error) {
             console.log(error);
         }
         const notes = JSON.parse(response);
-        console.log(notes);
         const noteRequest = req.body;
-        console.log(noteRequest);
         const newNoteId = notes.length + 1;
         const newNote = {
             id: newNoteId,
-            message: noteRequest.message
+            title: noteRequest.title,
+            text: noteRequest.text
         };
         notes.push(newNote);
         res.json(newNote);
-        fs.writeFile("db.json", JSON.stringify(notes, null, 2), function(err) {
+        fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(err) {
             if (err) throw err;
         });
     });
@@ -63,8 +63,8 @@ app.delete("/api/notes/:id", function(req, res) {
         }
         let notes = JSON.parse(response);
         if (deleteId <= notes.length) {
+            // Method to remove an element from an array obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
             res.json(notes.splice(deleteId-1,1));
-            console.log(notes);
             // Reassign the ids of all notes
             for (let i=0; i<notes.length; i++) {
                 notes[i].id = i+1;
